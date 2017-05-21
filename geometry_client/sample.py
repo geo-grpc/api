281ec7f8b2494ae8d08947cb790188c8ba15bf83
David Raleigh <davidraleigh@gmail.com>
2017-05-21 11:34:05 -0700

    Dockerfile building with proj4 and operator project working when called from python

--- geometry_client/sample.py
+++ geometry_client/sample.py
@@ -1,5 +1,6 @@
 import grpc
 from shapely.geometry import Polygon
+from shapely.geometry import LineString
 import geometry_operators_pb2
 import geometry_operators_pb2_grpc
 
@@ -10,12 +11,27 @@ serviceGeom = geometry_operators_pb2.ServiceGeometry(geometry_string=polygon.wkt
 opRequest = geometry_operators_pb2.OperatorRequest(left_geometry=serviceGeom,
                                                    operator_type='ExportToWkt')
 
+serviceSpatialReference = geometry_operators_pb2.ServiceSpatialReference(wkid=32632)
+outputSpatialReference = geometry_operators_pb2.ServiceSpatialReference(wkid=4326)
+polyline = LineString([( 500000,       0), (400000,  100000), (600000, -100000)])
+serviceGeomPolyline = geometry_operators_pb2.ServiceGeometry(geometry_string=polyline.wkt,
+                                                             geometry_encoding_type='wkt',
+                                                             spatial_reference=serviceSpatialReference)
+opRequestProject = geometry_operators_pb2.OperatorRequest(left_geometry=serviceGeomPolyline,
+                                                          operator_type='Project',
+                                                          operation_spatial_reference=outputSpatialReference)
+
 def run():
   channel = grpc.insecure_channel('localhost:8980')
   stub = geometry_operators_pb2_grpc.GeometryOperatorsStub(channel)
   response = stub.ExecuteOperation(opRequest)
-  print response
-  # print("Greeter client received: " + response.message)
+  # print response
+  print "Greeter client received:\n", response
 
+  response2 = stub.ExecuteOperation(opRequestProject)
+  print "Project response:\n", response2
+
+
+
+print "Greeter client sent:\n", serviceGeom
 run()
-print serviceGeom
