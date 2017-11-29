13fad93775c8eac5a7641b931320b458f3f3c65f
David Raleigh <davidraleigh@gmail.com>
2017-11-29 12:02:25 -0800

    first attempt

--- geometry_client/sample.py
+++ geometry_client/sample.py
@@ -1,54 +1,54 @@
 import grpc
 from shapely.geometry import Polygon
 from shapely.geometry import LineString
-import geometry_operators_pb2
-import geometry_operators_pb2_grpc
+from geometry_client.geometry_operators_pb2 import *
+import geometry_client.geometry_operators_pb2_grpc as geometry_grpc
 
 import sys
 
 polygon = Polygon([(0, 0), (1, 1), (1, 0)])
-serviceGeom = geometry_operators_pb2.ServiceGeometry(
+serviceGeom = ServiceGeometry(
     geometry_string=polygon.wkt,
-    geometry_encoding_type=geometry_operators_pb2.GeometryEncodingType.Value('wkt'))
+    geometry_encoding_type=GeometryEncodingType.Value('wkt'))
 
-opRequest = geometry_operators_pb2.OperatorRequest(left_geometry=serviceGeom,
-                                                   operator_type=geometry_operators_pb2.ServiceOperatorType.Value('ExportToWkt'))
+opRequest = OperatorRequest(left_geometry=serviceGeom,
+                                                   operator_type=ServiceOperatorType.Value('ExportToWkt'))
 
-serviceSpatialReference = geometry_operators_pb2.ServiceSpatialReference(wkid=32632)
-outputSpatialReference = geometry_operators_pb2.ServiceSpatialReference(wkid=4326)
+serviceSpatialReference = ServiceSpatialReference(wkid=32632)
+outputSpatialReference = ServiceSpatialReference(wkid=4326)
 polyline = LineString([(500000,       0), (400000,  100000), (600000, -100000)])
 
-serviceGeomPolyline = geometry_operators_pb2.ServiceGeometry(
+serviceGeomPolyline = ServiceGeometry(
     geometry_string=polyline.wkt,
-    geometry_encoding_type=geometry_operators_pb2.GeometryEncodingType.Value('wkt'),
+    geometry_encoding_type=GeometryEncodingType.Value('wkt'),
     spatial_reference=serviceSpatialReference)
 
-opRequestProject = geometry_operators_pb2.OperatorRequest(
+opRequestProject = OperatorRequest(
     left_geometry=serviceGeomPolyline,
-    operator_type=geometry_operators_pb2.ServiceOperatorType.Value('Project'),
+    operator_type=ServiceOperatorType.Value('Project'),
     operation_spatial_reference=outputSpatialReference)
 
 
 def run(address):
-    print "connect to address: ", address
-    print "create channel"
+    print("connect to address: ", address)
+    print("create channel")
     channel = grpc.insecure_channel(address)
 
-    print "make stub"
-    stub = geometry_operators_pb2_grpc.GeometryOperatorsStub(channel)
+    print("make stub")
+    stub = geometry_grpc.GeometryOperatorsStub(channel)
 
-    print "make wkt request"
+    print("make wkt request")
     response = stub.ExecuteOperation(opRequest)
     # print response
-    print "Client received wkt response:\n", response
+    print("Client received wkt response:\n", response)
 
-    print "make project request"
+    print("make project request")
     response2 = stub.ExecuteOperation(opRequestProject)
-    print "Client received project response:\n", response2
+    print("Client received project response:\n", response2)
 
 
 if __name__ == "__main__":
-    print "Greeter client sent:\n", serviceGeom
+    print("Greeter client sent:\n", serviceGeom)
     if len(sys.argv) == 1:
         address = 'localhost:8980'
     else:
