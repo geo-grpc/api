8262552861d3a788ed166d4d312bf8aa9defb997
David Raleigh <davidraleigh@gmail.com>
2017-11-30 12:27:47 -0800

    update proto and clarify tests

--- test/sample.py
+++ test/sample.py
@@ -22,10 +22,11 @@ import os
 import unittest
 
 import grpc
+from shapely.wkt import loads
 from shapely.geometry import LineString
 from shapely.geometry import Polygon
 from epl.geometry.geometry_operators_pb2 import *
-from epl.geometry import geometry_operators_pb2_grpc as geometry_grpc
+import epl.geometry.geometry_operators_pb2_grpc as geometry_grpc
 
 
 class TestBasic(unittest.TestCase):
@@ -40,28 +41,18 @@ class TestBasic(unittest.TestCase):
         # print("create channel")
         self.channel = grpc.insecure_channel(address)
 
-    def test_1(self):
+    def test_buffer(self):
         self.assertTrue(True)
         polygon = Polygon([(0, 0), (1, 1), (1, 0)])
-        serviceGeom = ServiceGeometry(
-            geometry_string=polygon.wkt,
-            geometry_encoding_type=GeometryEncodingType.Value('wkt'))
 
-        opRequest = OperatorRequest(left_geometry=serviceGeom, operator_type=ServiceOperatorType.Value('ExportToWkt'))
+        serviceGeom = ServiceGeometry()
+        serviceGeom.geometry_binary.extend([bytes(polygon.wkb)])
+        serviceGeom.geometry_encoding_type = GeometryEncodingType.Value('wkb')
 
-        serviceSpatialReference = ServiceSpatialReference(wkid=32632)
-        outputSpatialReference = ServiceSpatialReference(wkid=4326)
-        polyline = LineString([(500000,       0), (400000,  100000), (600000, -100000)])
+        opRequest = OperatorRequest(left_geometry=serviceGeom,
+                                    operator_type=ServiceOperatorType.Value('Buffer'),
+                                    buffer_distances=[1.2])
 
-        serviceGeomPolyline = ServiceGeometry(
-            geometry_string=polyline.wkt,
-            geometry_encoding_type=GeometryEncodingType.Value('wkt'),
-            spatial_reference=serviceSpatialReference)
-
-        opRequestProject = OperatorRequest(
-            left_geometry=serviceGeomPolyline,
-            operator_type=ServiceOperatorType.Value('Project'),
-            operation_spatial_reference=outputSpatialReference)
 
         print("make stub")
         stub = geometry_grpc.GeometryOperatorsStub(self.channel)
@@ -70,7 +61,31 @@ class TestBasic(unittest.TestCase):
         response = stub.ExecuteOperation(opRequest)
         # print response
         print("Client received wkt response:\n", response)
+        result_buffered = loads(response.geometry.geometry_string[0])
+        self.assertTrue(result_buffered.contains(polygon))
+        shapely_buffer = polygon.buffer(1.2)
+        self.assertAlmostEqual(shapely_buffer.area, result_buffered.area, 2)
+
+    def test_project(self):
+        stub = geometry_grpc.GeometryOperatorsStub(self.channel)
+        serviceSpatialReference = ServiceSpatialReference(wkid=32632)
+        outputSpatialReference = ServiceSpatialReference(wkid=4326)
+        polyline = LineString([(500000,       0), (400000,  100000), (600000, -100000)])
+
+        serviceGeomPolyline = ServiceGeometry(
+            geometry_string=[polyline.wkt],
+            geometry_encoding_type=GeometryEncodingType.Value('wkt'),
+            spatial_reference=serviceSpatialReference)
+
+        opRequestProject = OperatorRequest(
+            left_geometry=serviceGeomPolyline,
+            operator_type=ServiceOperatorType.Value('Project'),
+            operation_spatial_reference=outputSpatialReference)
 
         print("make project request")
         response2 = stub.ExecuteOperation(opRequestProject)
         print("Client received project response:\n", response2)
+
+        expected = "MULTILINESTRING ((9 0, 8.101251062924646 0.904618578893133, 9.898748937075354 -0.904618578893133))"
+        actual = response2.geometry.geometry_string[0]
+        self.assertEqual(expected, actual)
