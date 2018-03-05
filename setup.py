5d3f1e5dcb1b7652c24e0a6e06d6a285540fb50f
David Raleigh <davidraleigh@gmail.com>
2018-03-04 17:58:02 -0800

    fixed imports to work for tests

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ kwargs = {
 
 clssfrs = [
     "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
