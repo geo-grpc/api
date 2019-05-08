1ba274c36b580fb47383feb826c77e019697a4d4
David Raleigh <davidraleigh@gmail.com>
2019-05-08 14:13:25 -0400

    moving protos

---
+++ generate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+protoc -I proto/ proto/epl/protobuf/geometry.proto --go_out=$GOPATH/src
+protoc -I proto/ proto/epl/protobuf/query.proto --go_out=$GOPATH/src
+protoc -I proto/ proto/epl/protobuf/stac.proto --go_out=$GOPATH/src
+protoc -I proto/ proto/epl/protobuf/naip.proto --go_out=$GOPATH/src
+protoc -I proto/ proto/epl/protobuf/stac_service.proto --go_out=plugins=grpc:$GOPATH/src
+protoc -I proto/ proto/epl/protobuf/geometry_service.proto --go_out=plugins=grpc:$GOPATH/src
+
+python -mgrpc_tools.protoc -I=./proto --python_out=./python \
+    ./proto/epl/protobuf/geometry.proto \
+    ./proto/epl/protobuf/query.proto \
+    ./proto/epl/protobuf/stac.proto \
+    ./proto/epl/protobuf/naip.proto \
+
+python -mgrpc_tools.protoc -I=./proto --grpc_python_out=./python \
+    ./proto/epl/protobuf/geometry_service.proto \
+    ./proto/epl/protobuf/stac_service.proto
