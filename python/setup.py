20b2d2fd58b7cc4d7840c93a40b82418d681f93a
David Raleigh <davidraleigh@gmail.com>
2019-08-14 12:49:51 -0500

    collection request

--- python/setup.py
+++ python/setup.py
@@ -16,7 +16,7 @@ kwargs = {
     'author': 'David Raleigh',
     'author_email': 'davidraleigh@gmail.com',
     'license': 'Apache 2.0',
-    'version': '0.3.20',
+    'version': '0.3.22',
     'namespace_package': ['epl'],
     'python_requires': '>3.5.2',
     'packages': ['epl.protobuf'],
