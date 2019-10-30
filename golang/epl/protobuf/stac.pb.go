cf5491ccd243f11864a8350d471b61142e00ab54
David Raleigh <davidraleigh@gmail.com>
2019-10-30 15:39:41 -0400

    webp

--- golang/epl/protobuf/stac.pb.go
+++ golang/epl/protobuf/stac.pb.go
@@ -43,6 +43,7 @@ const (
 	AssetType_OVERVIEW      AssetType = 15
 	AssetType_JSON          AssetType = 16
 	AssetType_HTML          AssetType = 17
+	AssetType_WEBP          AssetType = 18
 )
 
 var AssetType_name = map[int32]string{
@@ -64,6 +65,7 @@ var AssetType_name = map[int32]string{
 	15: "OVERVIEW",
 	16: "JSON",
 	17: "HTML",
+	18: "WEBP",
 }
 var AssetType_value = map[string]int32{
 	"UNKNOWN_ASSET": 0,
@@ -84,13 +86,14 @@ var AssetType_value = map[string]int32{
 	"OVERVIEW":      15,
 	"JSON":          16,
 	"HTML":          17,
+	"WEBP":          18,
 }
 
 func (x AssetType) String() string {
 	return proto.EnumName(AssetType_name, int32(x))
 }
 func (AssetType) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{0}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{0}
 }
 
 type CloudPlatform int32
@@ -119,7 +122,7 @@ func (x CloudPlatform) String() string {
 	return proto.EnumName(CloudPlatform_name, int32(x))
 }
 func (CloudPlatform) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{1}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{1}
 }
 
 type Sar_Platform int32
@@ -145,7 +148,7 @@ func (x Sar_Platform) String() string {
 	return proto.EnumName(Sar_Platform_name, int32(x))
 }
 func (Sar_Platform) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{5, 0}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{5, 0}
 }
 
 type Sar_Instrument int32
@@ -165,7 +168,7 @@ func (x Sar_Instrument) String() string {
 	return proto.EnumName(Sar_Instrument_name, int32(x))
 }
 func (Sar_Instrument) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{5, 1}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{5, 1}
 }
 
 type Eo_Platform int32
@@ -216,7 +219,7 @@ func (x Eo_Platform) String() string {
 	return proto.EnumName(Eo_Platform_name, int32(x))
 }
 func (Eo_Platform) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{6, 0}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{6, 0}
 }
 
 type Eo_Instrument int32
@@ -258,7 +261,7 @@ func (x Eo_Instrument) String() string {
 	return proto.EnumName(Eo_Instrument_name, int32(x))
 }
 func (Eo_Instrument) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{6, 1}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{6, 1}
 }
 
 type Eo_Constellation int32
@@ -292,7 +295,7 @@ func (x Eo_Constellation) String() string {
 	return proto.EnumName(Eo_Constellation_name, int32(x))
 }
 func (Eo_Constellation) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{6, 2}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{6, 2}
 }
 
 type Eo_Band int32
@@ -355,7 +358,7 @@ func (x Eo_Band) String() string {
 	return proto.EnumName(Eo_Band_name, int32(x))
 }
 func (Eo_Band) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{6, 3}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{6, 3}
 }
 
 type Landsat_ProcessingLevel int32
@@ -390,7 +393,7 @@ func (x Landsat_ProcessingLevel) String() string {
 	return proto.EnumName(Landsat_ProcessingLevel_name, int32(x))
 }
 func (Landsat_ProcessingLevel) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{8, 0}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{8, 0}
 }
 
 // *
@@ -435,7 +438,7 @@ func (m *StacItem) Reset()         { *m = StacItem{} }
 func (m *StacItem) String() string { return proto.CompactTextString(m) }
 func (*StacItem) ProtoMessage()    {}
 func (*StacItem) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{0}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{0}
 }
 func (m *StacItem) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_StacItem.Unmarshal(m, b)
@@ -600,7 +603,7 @@ func (m *StacRequest) Reset()         { *m = StacRequest{} }
 func (m *StacRequest) String() string { return proto.CompactTextString(m) }
 func (*StacRequest) ProtoMessage()    {}
 func (*StacRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{1}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{1}
 }
 func (m *StacRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_StacRequest.Unmarshal(m, b)
@@ -757,7 +760,7 @@ func (m *Asset) Reset()         { *m = Asset{} }
 func (m *Asset) String() string { return proto.CompactTextString(m) }
 func (*Asset) ProtoMessage()    {}
 func (*Asset) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{2}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{2}
 }
 func (m *Asset) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Asset.Unmarshal(m, b)
@@ -859,7 +862,7 @@ func (m *DatetimeRange) Reset()         { *m = DatetimeRange{} }
 func (m *DatetimeRange) String() string { return proto.CompactTextString(m) }
 func (*DatetimeRange) ProtoMessage()    {}
 func (*DatetimeRange) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{3}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{3}
 }
 func (m *DatetimeRange) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_DatetimeRange.Unmarshal(m, b)
@@ -909,7 +912,7 @@ func (m *StacDbResponse) Reset()         { *m = StacDbResponse{} }
 func (m *StacDbResponse) String() string { return proto.CompactTextString(m) }
 func (*StacDbResponse) ProtoMessage()    {}
 func (*StacDbResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{4}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{4}
 }
 func (m *StacDbResponse) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_StacDbResponse.Unmarshal(m, b)
@@ -970,7 +973,7 @@ func (m *Sar) Reset()         { *m = Sar{} }
 func (m *Sar) String() string { return proto.CompactTextString(m) }
 func (*Sar) ProtoMessage()    {}
 func (*Sar) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{5}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{5}
 }
 func (m *Sar) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Sar.Unmarshal(m, b)
@@ -1033,7 +1036,7 @@ func (m *Eo) Reset()         { *m = Eo{} }
 func (m *Eo) String() string { return proto.CompactTextString(m) }
 func (*Eo) ProtoMessage()    {}
 func (*Eo) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{6}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{6}
 }
 func (m *Eo) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Eo.Unmarshal(m, b)
@@ -1171,7 +1174,7 @@ func (m *EoRequest) Reset()         { *m = EoRequest{} }
 func (m *EoRequest) String() string { return proto.CompactTextString(m) }
 func (*EoRequest) ProtoMessage()    {}
 func (*EoRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{7}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{7}
 }
 func (m *EoRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_EoRequest.Unmarshal(m, b)
@@ -1285,7 +1288,7 @@ func (m *Landsat) Reset()         { *m = Landsat{} }
 func (m *Landsat) String() string { return proto.CompactTextString(m) }
 func (*Landsat) ProtoMessage()    {}
 func (*Landsat) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{8}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{8}
 }
 func (m *Landsat) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Landsat.Unmarshal(m, b)
@@ -1363,7 +1366,7 @@ func (m *LandsatRequest) Reset()         { *m = LandsatRequest{} }
 func (m *LandsatRequest) String() string { return proto.CompactTextString(m) }
 func (*LandsatRequest) ProtoMessage()    {}
 func (*LandsatRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_stac_8c776108a2281c45, []int{9}
+	return fileDescriptor_stac_e68be7cc80a4980d, []int{9}
 }
 func (m *LandsatRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_LandsatRequest.Unmarshal(m, b)
@@ -1448,16 +1451,16 @@ func init() {
 	proto.RegisterEnum("epl.protobuf.Landsat_ProcessingLevel", Landsat_ProcessingLevel_name, Landsat_ProcessingLevel_value)
 }
 
-func init() { proto.RegisterFile("epl/protobuf/stac.proto", fileDescriptor_stac_8c776108a2281c45) }
+func init() { proto.RegisterFile("epl/protobuf/stac.proto", fileDescriptor_stac_e68be7cc80a4980d) }
 
-var fileDescriptor_stac_8c776108a2281c45 = []byte{
-	// 2056 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_stac_e68be7cc80a4980d = []byte{
+	// 2062 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xd4, 0x58, 0xcf, 0x76, 0xdb, 0xc6,
 	0xf5, 0x0e, 0xff, 0x83, 0x97, 0x22, 0x35, 0x9e, 0x9f, 0x13, 0x53, 0x72, 0xe2, 0xe8, 0xc7, 0x34,
 	0xa7, 0xae, 0xd3, 0x48, 0x16, 0x6d, 0xa7, 0x8a, 0x9b, 0xf6, 0x98, 0xa4, 0x20, 0x1a, 0x2e, 0x08,
 	0xf2, 0x0c, 0x20, 0xc9, 0x27, 0x1b, 0x1c, 0x88, 0x18, 0xd1, 0x4c, 0x40, 0x00, 0x01, 0x40, 0xc9,
-	0xea, 0x4a, 0xdb, 0xee, 0xdb, 0x17, 0x68, 0x9f, 0xa1, 0x8b, 0x3e, 0x42, 0xb7, 0x5d, 0x74, 0xdd,
-	0x57, 0xe8, 0x1b, 0xf4, 0xcc, 0x0c, 0x00, 0x81, 0xd4, 0xdf, 0xe3, 0x5d, 0x56, 0x9a, 0x3b, 0xf7,
+	0xea, 0x4a, 0xdb, 0xee, 0xdb, 0x17, 0x68, 0x9f, 0xa1, 0x8b, 0x3e, 0x42, 0xb7, 0x5d, 0xf4, 0x21,
+	0xba, 0xeb, 0x1b, 0xf4, 0xcc, 0x0c, 0x00, 0x81, 0xd4, 0xdf, 0xe3, 0x5d, 0x56, 0x9a, 0x3b, 0xf7,
 	0xfb, 0x66, 0x2e, 0xee, 0xdc, 0xfb, 0xcd, 0x50, 0xf0, 0x80, 0xfa, 0xce, 0x96, 0x1f, 0x78, 0x91,
 	0x77, 0x34, 0x3f, 0xde, 0x0a, 0x23, 0x6b, 0xbc, 0xc9, 0x2d, 0xbc, 0x42, 0x7d, 0x67, 0x33, 0x71,
 	0xac, 0x7f, 0x3e, 0xf1, 0xbc, 0x89, 0x43, 0x2f, 0x90, 0xd1, 0x74, 0x46, 0xc3, 0xc8, 0x9a, 0xf9,
@@ -1573,12 +1576,12 @@ var fileDescriptor_stac_8c776108a2281c45 = []byte{
 	0x2a, 0xaa, 0x8a, 0x9b, 0x6b, 0x6f, 0x0f, 0x01, 0x73, 0xb0, 0x5d, 0xcd, 0xf6, 0xd3, 0xa7, 0x4f,
 	0x51, 0x8d, 0x11, 0xd8, 0x4a, 0x2b, 0x6c, 0x60, 0xbc, 0x35, 0x50, 0x9d, 0x0d, 0x46, 0x5a, 0x1f,
 	0x35, 0xf8, 0x1d, 0x77, 0x20, 0x93, 0x03, 0x45, 0x3e, 0x44, 0xab, 0x3c, 0x5a, 0x7d, 0xa8, 0x21,
-	0xc4, 0x46, 0xaf, 0x8d, 0x81, 0x8a, 0xee, 0x3d, 0x79, 0x0d, 0xf5, 0x85, 0xdf, 0x3a, 0x78, 0x1d,
-	0x3e, 0x49, 0xaf, 0x34, 0x75, 0xb8, 0xbf, 0x9b, 0x7d, 0x04, 0x54, 0xa0, 0xd0, 0x39, 0xd4, 0x51,
-	0x8e, 0x0d, 0xfa, 0x3d, 0x56, 0x55, 0x55, 0x28, 0x75, 0xbe, 0xdf, 0x27, 0x32, 0x2a, 0x74, 0x75,
-	0x40, 0x63, 0x6f, 0xb6, 0x90, 0xdb, 0x6e, 0x95, 0xfd, 0x14, 0x18, 0x31, 0x6b, 0x94, 0xfb, 0xfe,
-	0xab, 0xc9, 0x34, 0x7a, 0x37, 0x3f, 0xda, 0x1c, 0x7b, 0xb3, 0xad, 0x09, 0xf5, 0xbe, 0x9e, 0x04,
-	0xfe, 0x78, 0xcb, 0xf2, 0xa7, 0x5b, 0x13, 0xcf, 0xb1, 0xdc, 0xc9, 0x56, 0xf6, 0x1f, 0x88, 0x7f,
-	0xcd, 0x17, 0x75, 0x63, 0xd4, 0x3d, 0x2a, 0xf3, 0x89, 0x67, 0xff, 0x0b, 0x00, 0x00, 0xff, 0xff,
-	0x43, 0x36, 0x6e, 0xa3, 0x1e, 0x15, 0x00, 0x00,
+	0xc4, 0x46, 0xaf, 0x8d, 0x81, 0x8a, 0xee, 0xb1, 0xd1, 0xa1, 0xdc, 0x1d, 0x21, 0xfc, 0xe4, 0x35,
+	0xd4, 0x17, 0x7e, 0xf5, 0xe0, 0x75, 0xf8, 0x24, 0xbd, 0xdc, 0xd4, 0xe1, 0xfe, 0x6e, 0xf6, 0x39,
+	0x50, 0x81, 0x42, 0xe7, 0x50, 0x47, 0x39, 0x36, 0xe8, 0xf7, 0x58, 0x7d, 0x55, 0xa1, 0xd4, 0xf9,
+	0x7e, 0x9f, 0xc8, 0xa8, 0xd0, 0xd5, 0x01, 0x8d, 0xbd, 0xd9, 0x42, 0x96, 0xbb, 0x55, 0xf6, 0xa3,
+	0x60, 0xc4, 0xac, 0x51, 0xee, 0xfb, 0xaf, 0x26, 0xd3, 0xe8, 0xdd, 0xfc, 0x68, 0x73, 0xec, 0xcd,
+	0xb6, 0x26, 0xd4, 0xfb, 0x7a, 0x12, 0xf8, 0xe3, 0x2d, 0xcb, 0x9f, 0x6e, 0x4d, 0x3c, 0xc7, 0x72,
+	0x27, 0x5b, 0xd9, 0x7f, 0x25, 0xfe, 0x35, 0x5f, 0xd4, 0x8d, 0x51, 0xf7, 0xa8, 0xcc, 0x27, 0x9e,
+	0xfd, 0x2f, 0x00, 0x00, 0xff, 0xff, 0xcd, 0x52, 0x50, 0x00, 0x28, 0x15, 0x00, 0x00,
 }
