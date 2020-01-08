1cf9c0f1a3e224dc93b6d0ec1a3b628b8e9b38db
sunny-g <sunny.gonna@gmail.com>
2020-01-08 17:35:12 -0600

    adds basic test

--- rust/tests/mod.rs
+++ rust/tests/mod.rs
@@ -1,39 +1,46 @@
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    pub static ref TOKEN: String = {
+        let token =
+            std::env::var("TOKEN").expect("Failed to retrieve environment variable `TOKEN`");
+        format!("Bearer {}", token)
+    };
+}
+
 #[cfg(test)]
 mod tests {
-    use futures03::{compat::*, pin_mut, task::Poll, Stream};
+    use super::TOKEN;
+    use futures03::{compat::Stream01CompatExt, executor, pin_mut, task::Poll, Stream, StreamExt};
     use futures03_test::{stream::StreamTestExt, task::noop_context};
-    use grpcio::{ChannelBuilder, EnvBuilder, Error};
+    use grpcio::{CallOption, ChannelBuilder, EnvBuilder, Error, MetadataBuilder};
     use stac_proto::*;
 
     #[test]
     fn it_works() -> Result<(), Error> {
+        // setup
         let env = std::sync::Arc::new(EnvBuilder::new().build());
-        let channel = ChannelBuilder::new(env).connect("https://eap.nearspacelabs.net");
+        let channel = ChannelBuilder::new(env).connect("api.nearspacelabs.net:9090");
         let client = StacServiceClient::new(channel);
 
-        let res_stream = client
-            .search(&StacRequest::default())?
-            .compat()
-            .interleave_pending();
+        // headers
+        let mut metadata_builder = MetadataBuilder::new();
+        metadata_builder
+            .add_str("Authorization", TOKEN.as_str())
+            .unwrap_or_else(|_| panic!("unable to set token as an `Authorization` header"));
+        let opts = CallOption::default().headers(metadata_builder.build());
 
-        pin_mut!(res_stream);
-        let mut ctx = noop_context();
+        // result stream into vec
+        let res_stream = client.search_opt(&StacRequest::default(), opts)?.compat();
+        let mut vec = executor::block_on(res_stream.collect::<Vec<Result<StacItem, Error>>>());
 
-        let first = res_stream.as_mut().poll_next(&mut ctx);
-        assert!(first.is_pending());
+        assert!(!vec.is_empty(), "Failed to retrieve any stac items");
+        for (count, item) in vec.into_iter().enumerate() {
+            assert!(item.is_ok(), format!("Erroneous result: {:?}", item));
+            println!("stac item ID: {:?}", item.unwrap().get_id());
+        }
 
-        match res_stream.as_mut().poll_next(&mut ctx) {
-            Poll::Ready(Some(Ok(stac_item))) => {
-                println!("stac item: {:?}", stac_item);
-                assert!(true);
-            }
-            Poll::Ready(Some(Err(err))) => {
-                assert!(false, format!("result stream produced error: {}", err));
-            }
-            Poll::Pending => assert!(false, "result stream still pending"),
-            _ => assert!(false, "result stream errored out"),
-        };
         Ok(())
-        //        println!("resulting stac item: {:?}", res.unwrap());
     }
 }
