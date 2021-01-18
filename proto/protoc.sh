330dc6bb79039256747d60638137a6fb4579db2d
David Raleigh <davidraleigh@gmail.com>
2021-01-18 18:03:39 -0500

    new dir structure

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -2,13 +2,13 @@
 
 MONO_PATH=/defs/src/github.com/geo-grpc/api
 #PYTHON
-protoc -I/opt/include -I="$MONO_PATH"/proto --python_out="$MONO_PATH"/python \
+protoc -I/opt/include -I="$MONO_PATH"/proto --python_out="$MONO_PATH"/python/epl_protobuf \
   "$MONO_PATH"/proto/epl/protobuf/v1/geometry.proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/query.proto  \
   "$MONO_PATH"/proto/epl/protobuf/v1/stac.proto
 
 protoc -I/opt/include -I="$MONO_PATH"/proto --plugin=protoc-gen-grpc_python="$(command -v grpc_python_plugin)" \
-  --grpc_python_out="$MONO_PATH"/python \
+  --grpc_python_out="$MONO_PATH"/python/epl_protobuf \
   "$MONO_PATH"/proto/epl/protobuf/v1/geometry_service.proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/stac_service.proto
 #PYTHON
@@ -25,7 +25,7 @@ protoc -I/opt/include -I "$MONO_PATH"/proto --go_out=plugins=grpc:/defs/src \
 #GO
 
 #CPP
-protoc -I/opt/include -I "$MONO_PATH"/proto --cpp_out="$MONO_PATH"/cpp \
+protoc -I/opt/include -I "$MONO_PATH"/proto --cpp_out="$MONO_PATH"/cpp/protobuf-lib \
   -I "$MONO_PATH"/proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/geometry.proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/query.proto  \
@@ -33,7 +33,7 @@ protoc -I/opt/include -I "$MONO_PATH"/proto --cpp_out="$MONO_PATH"/cpp \
   "$MONO_PATH"/proto/epl/protobuf/v1/geometry_service.proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/stac_service.proto
 
-protoc -I/opt/include -I "$MONO_PATH"/proto --grpc_out="$MONO_PATH"/cpp --plugin=protoc-gen-grpc="$(command -v grpc_cpp_plugin)" \
+protoc -I/opt/include -I "$MONO_PATH"/proto --grpc_out="$MONO_PATH"/cpp/protobuf-lib --plugin=protoc-gen-grpc="$(command -v grpc_cpp_plugin)" \
   -I "$MONO_PATH"/proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/geometry_service.proto \
   "$MONO_PATH"/proto/epl/protobuf/v1/stac_service.proto
