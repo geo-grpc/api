2db5bff9b8566eed51a7d3f46ea5667e6c5de381
David Raleigh <davidraleigh@gmail.com>
2021-03-18 16:10:24 -0700

    go-geom operator

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/geo-grpc/api
 
 require (
-	github.com/golang/protobuf v1.2.0
-	github.com/twpayne/go-geom v1.3.6
+	github.com/davidraleigh/go-geom v1.3.8
+	github.com/golang/protobuf v1.4.1
 	golang.org/x/net v0.0.0-20191003171128-d98b1b443823
-	google.golang.org/grpc v1.19.0
+	google.golang.org/grpc v1.27.0
 )
 
 go 1.13
--- golang/epl/geometry/operators.go
+++ golang/epl/geometry/operators.go
@@ -5,12 +5,22 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	gogeom "github.com/davidraleigh/go-geom"
+	gogeomewkb "github.com/davidraleigh/go-geom/encoding/ewkb"
+	gogeomgeojson "github.com/davidraleigh/go-geom/encoding/geojson"
+	gogeomwkb "github.com/davidraleigh/go-geom/encoding/wkb"
+	gogeomwkt "github.com/davidraleigh/go-geom/encoding/wkt"
 	eplpbv1 "github.com/geo-grpc/api/golang/epl/protobuf/v1"
-	gogeom "github.com/twpayne/go-geom"
-	gogeomewkb "github.com/twpayne/go-geom/encoding/ewkb"
-	gogeomwkb "github.com/twpayne/go-geom/encoding/wkb"
 	"google.golang.org/grpc"
 )
+
+type ExecuteBlocking func(eplpbv1.GeometryRequest) (error, gogeom.T)
+
+type ChainRequest struct {
+	geometryRequest *eplpbv1.GeometryRequest
+	gs *Service
+	err *error
+}
 
 type Service struct {
 	ClientV1 eplpbv1.GeometryServiceClient
@@ -18,49 +28,143 @@ type Service struct {
 	Proj4326 *eplpbv1.ProjectionData
 }
 
-func (gs *Service) Buffer(t gogeom.T, params *eplpbv1.Params_Buffer, resultEpsg ...int32) (error, gogeom.T) {
+func (c *ChainRequest) ExecuteBlockingSingle() (*error, gogeom.T) {
+	if c.err != nil {
+		return c.err, nil
+	}
+
+	requestResult, err := c.gs.ClientV1.Operate(context.Background(), c.geometryRequest)
+	if err != nil {
+		newerr := errors.New(fmt.Sprintf("geometry service error with value:\n%v", err.Error()))
+		return &newerr, nil
+	}
+
+	if requestResult.GetGeometry() == nil || requestResult.GetGeometry().Proj == nil {
+		// TODO does this ever occur?
+		err = errors.New("geometry Result incomplete")
+		return &err, nil
+	}
+
+	return c.gs.ParseToGoGeom(requestResult.GetGeometry())
+}
+
+func (gs *Service) SingleInputChain(t gogeom.T, request *eplpbv1.GeometryRequest, c *ChainRequest) *ChainRequest {
+	if c != nil && c.err != nil {
+		return c
+	}
+
+	newChain := &ChainRequest{
+		gs:              gs,
+	}
+
+	if request.GetGeometry() != nil {
+
+	} else if c != nil {
+		request.SetLeftGeometryRequest(c.geometryRequest)
+	} else if t != nil {
+		err, geometryData := ParseToGeometryData(t)
+		if err != nil {
+			newChain.err = &err
+			return newChain
+		}
+		request.SetGeometry(geometryData)
+	} else if request.GetGeometry() == nil && request.GetGeometryRequest() == nil {
+		err := errors.New("either geometry or chain request must be defined")
+		newChain.err = &err
+		return newChain
+	}
+
+	request.ResultEncoding = eplpbv1.Encoding_EWKB
+
+	newChain.geometryRequest = request
+	return newChain
+}
+
+func (gs *Service) Buffer(t gogeom.T, params *eplpbv1.Params_Buffer) (*error, gogeom.T) {
+	chain := gs.BufferChain(t, nil, params)
+	if chain.err != nil {
+		return chain.err, nil
+	}
+	return chain.ExecuteBlockingSingle()
+}
+
+func (gs *Service) BufferChain(t gogeom.T, c *ChainRequest, params *eplpbv1.Params_Buffer) *ChainRequest {
 	request := &eplpbv1.GeometryRequest{
 		Operator:             eplpbv1.OperatorType_BUFFER,
 		Params:               &eplpbv1.GeometryRequest_BufferParams{BufferParams:params},
 	}
 
-	return gs.SingleInputRequest(t, request, resultEpsg)
+	return gs.SingleInputChain(t, request, c)
 }
 
-func (gs *Service) GeodesicBuffer(t gogeom.T, params *eplpbv1.Params_GeodeticBuffer, resultEpsg ...int32) (error, gogeom.T) {
+func (gs *Service) GeodesicBuffer(t gogeom.T, params *eplpbv1.Params_GeodeticBuffer) (*error, gogeom.T) {
+	chain := gs.GeodesicBufferChain(t, nil, params)
+	if chain.err != nil {
+		return chain.err, nil
+	}
+	return chain.ExecuteBlockingSingle()
+}
+
+func (gs *Service) GeodesicBufferChain(t gogeom.T, c *ChainRequest, params *eplpbv1.Params_GeodeticBuffer) *ChainRequest {
 	request := &eplpbv1.GeometryRequest{
 		Operator:             eplpbv1.OperatorType_GEODESIC_BUFFER,
 		Params:               &eplpbv1.GeometryRequest_GeodeticBufferParams{GeodeticBufferParams:params},
 	}
 
-	return gs.SingleInputRequest(t, request, resultEpsg)
+	return gs.SingleInputChain(t, request, c)
 }
 
+func (gs *Service) Simplify(t gogeom.T, params *eplpbv1.Params_Simplify) (*error, gogeom.T) {
+	chain := gs.SimplifyChain(t, nil, params)
+	if chain.err != nil {
+		return chain.err, nil
+	}
+	return chain.ExecuteBlockingSingle()
+}
 
-func (gs *Service) Simplify(t gogeom.T, params *eplpbv1.Params_Simplify, resultEpsg ...int32) (error, gogeom.T) {
+func (gs *Service) SimplifyChain(t gogeom.T, c *ChainRequest, params *eplpbv1.Params_Simplify) *ChainRequest {
 	request := &eplpbv1.GeometryRequest{
 		Operator:             eplpbv1.OperatorType_SIMPLIFY,
 		Params:               &eplpbv1.GeometryRequest_SimplifyParams{SimplifyParams:params},
 	}
 
-	return gs.SingleInputRequest(t, request, resultEpsg)
+	return gs.SingleInputChain(t, request, c)
 }
 
-func (gs *Service) ConvexHull(t gogeom.T, resultEpsg ...int32) (error, gogeom.T) {
+func (gs *Service) ConvexHull(t gogeom.T) (*error, gogeom.T) {
+	chain := gs.ConvexHullChain(t, nil)
+	if chain.err != nil {
+		return chain.err, nil
+	}
+	return chain.ExecuteBlockingSingle()
+}
+
+func (gs *Service) ConvexHullChain(t gogeom.T, c *ChainRequest) *ChainRequest {
 	request := &eplpbv1.GeometryRequest{
 		Operator:             eplpbv1.OperatorType_CONVEX_HULL,
 	}
 
-	return gs.SingleInputRequest(t, request, resultEpsg)
+	return gs.SingleInputChain(t, request, c)
 }
 
-func (gs *Service) Project(t gogeom.T, resultEpsg int32) (error, gogeom.T) {
+func (gs *Service) Project(t gogeom.T, resultEpsg int32) (*error, gogeom.T) {
+	chain := gs.ProjectChain(t, nil, resultEpsg)
+	if chain.err != nil {
+		return chain.err, nil
+	}
+	return chain.ExecuteBlockingSingle()
+}
+
+func (gs *Service) ProjectChain(t gogeom.T, c *ChainRequest, resultEpsg int32) *ChainRequest {
 	request := &eplpbv1.GeometryRequest{
 		Operator:             eplpbv1.OperatorType_PROJECT,
 	}
-	epsg :=[]int32{resultEpsg}
 
-	return gs.SingleInputRequest(t, request, epsg)
+	request.ResultProj = &eplpbv1.ProjectionData{
+		Definition:           &eplpbv1.ProjectionData_Epsg{Epsg:resultEpsg},
+	}
+
+	return gs.SingleInputChain(t, request, c)
 }
 
 func (gs *Service) Contains(left gogeom.T, right gogeom.T) (error, bool) {
@@ -111,31 +215,31 @@ func (gs *Service) Disjoint(left gogeom.T, right gogeom.T) (error, bool) {
 	return gs.RelationalRequest(left, right, request)
 }
 
-func (gs *Service) Intersection(left gogeom.T, right gogeom.T, resultEpsg ...int32) (error, gogeom.T) {
+func (gs *Service) Intersection(left gogeom.T, right gogeom.T) (error, gogeom.T) {
 	request := &eplpbv1.GeometryRequest{
 		Operator:eplpbv1.OperatorType_INTERSECTION,
 	}
 
-	return gs.TwoInputRequest(left, right, request, resultEpsg)
+	return gs.TwoInputRequest(left, right, request)
 }
 
-func (gs *Service) Difference(left gogeom.T, right gogeom.T, resultEpsg ...int32) (error, gogeom.T) {
+func (gs *Service) Difference(left gogeom.T, right gogeom.T) (error, gogeom.T) {
 	request := &eplpbv1.GeometryRequest{
 		Operator:eplpbv1.OperatorType_DIFFERENCE,
 	}
 
-	return gs.TwoInputRequest(left, right, request, resultEpsg)
+	return gs.TwoInputRequest(left, right, request)
 }
 
-func (gs *Service) SymmetricDifference(left gogeom.T, right gogeom.T, resultEpsg ...int32) (error, gogeom.T) {
+func (gs *Service) SymmetricDifference(left gogeom.T, right gogeom.T) (error, gogeom.T) {
 	request := &eplpbv1.GeometryRequest{
 		Operator:eplpbv1.OperatorType_SYMMETRIC_DIFFERENCE,
 	}
 
-	return gs.TwoInputRequest(left, right, request, resultEpsg)
+	return gs.TwoInputRequest(left, right, request)
 }
 
-func (gs *Service) TwoInputRequest(left gogeom.T, right gogeom.T, request *eplpbv1.GeometryRequest, resultEpsg []int32) (error, gogeom.T) {
+func (gs *Service) TwoInputRequest(left gogeom.T, right gogeom.T, request *eplpbv1.GeometryRequest, resultEpsg ...int32) (error, gogeom.T) {
 	err, leftGeom := ParseToGeometryData(left)
 	if err != nil {
 		return err, nil
@@ -174,34 +278,16 @@ func (gs *Service) RelationalRequest(left gogeom.T, right gogeom.T, request *epl
 
 	requestResult, err := gs.ClientV1.Operate(context.Background(), request)
 	if err != nil {
-		return errors.New(fmt.Sprintf("geometry service error with value:\n%v", err.Error())), nil
+		return errors.New(fmt.Sprintf("geometry service error with value:\n%v", err.Error())), false
 	}
 
 	return nil, requestResult.GetSpatialRelationship()
 }
 
-func (gs *Service) SingleInputRequest(t gogeom.T, request *eplpbv1.GeometryRequest, resultEpsg []int32) (error, gogeom.T) {
-	err, geometryData := ParseToGeometryData(t)
-	if err != nil {
-		return err, nil
-	}
-	request.SetGeometry(geometryData)
-
-	if len(resultEpsg) > 0 {
-		request.ResultProj = &eplpbv1.ProjectionData{
-			Definition:           &eplpbv1.ProjectionData_Epsg{Epsg:resultEpsg[0]},
-		}
-	}
-
-	request.ResultEncoding = eplpbv1.Encoding_EWKB
-
-	return gs.GeometryResultRequest(request)
-}
-
 func (gs *Service) GeometryResultRequest(request *eplpbv1.GeometryRequest) (error, gogeom.T) {
 	requestResult, err := gs.ClientV1.Operate(context.Background(), request)
 	if err != nil {
-		return errors.New(fmt.Sprintf("geometry service error with value:\n%v", err.Error())), nil
+		return err, nil
 	}
 
 	if requestResult.GetGeometry() == nil || requestResult.GetGeometry().Proj == nil || requestResult.GetGeometry().GetWkb() == nil {
@@ -210,7 +296,8 @@ func (gs *Service) GeometryResultRequest(request *eplpbv1.GeometryRequest) (erro
 		return err, nil
 	}
 
-	return ParseToGoGeom(requestResult.GetGeometry())
+	err2, geom := gs.ParseToGoGeom(requestResult.GetGeometry())
+	return *err2, geom
 }
 
 func ParseToGeometryData(t gogeom.T) (error, *eplpbv1.GeometryData) {
@@ -232,19 +319,37 @@ func ParseToGeometryData(t gogeom.T) (error, *eplpbv1.GeometryData) {
 	}
 }
 
-func ParseToGoGeom(geometryData *eplpbv1.GeometryData) (error, gogeom.T) {
-	if geometryData.GetEwkb() == nil {
-		return errors.New("only parsing ewkb"), nil
-	}
-
-	geometry, err := gogeomewkb.Unmarshal(geometryData.GetEwkb())
-	if err != nil {
-		return err, nil
-	}
-
+func (gs *Service) ParseToGoGeom(geometryData *eplpbv1.GeometryData) (*error, gogeom.T) {
 	if geometryData.Proj.GetEpsg() == 0 {
-		return errors.New("geometryData needs epsg defined"), nil
+		err := errors.New("geometryData needs epsg defined")
+		return &err, nil
 	}
+
+	var geometry gogeom.T
+	var err error
+	if geometryData.GetEwkb() != nil {
+		geometry, err = gogeomewkb.Unmarshal(geometryData.GetEwkb())
+	} else if geometryData.GetWkb() != nil {
+		geometry, err = gogeomwkb.Unmarshal(geometryData.GetWkb())
+	} else if len(geometryData.GetWkt()) != 0 {
+		geometry, err = gogeomwkt.Unmarshal(geometryData.GetWkt())
+	} else if len(geometryData.GetGeojson()) != 0 {
+		err = gogeomgeojson.Unmarshal([]byte(geometryData.GetGeojson()), &geometry)
+	} else if geometryData.GetEsriShape() != nil {
+		emptyRequest := eplpbv1.GeometryRequest{}
+		emptyRequest.SetLeftGeometry(geometryData)
+		var err2 *error
+		err2, geometry = gs.SingleInputChain(nil, &emptyRequest, nil).ExecuteBlockingSingle()
+		err = *err2
+	} else {
+		err = errors.New("no geometry information in ewkb, wkb, geojson, or wkt")
+	}
+
+	if err != nil {
+		return &err, nil
+	}
+
+	geometry.SetSRIDEx(int(geometryData.Proj.GetEpsg()))
 
 	return nil, geometry
 }
