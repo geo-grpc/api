06048763278931e737afe7ce0a35d097ed1ad5ff
David Raleigh <davidraleigh@gmail.com>
2021-03-21 13:32:58 -0700

    java version

--- docker/generate.sh
+++ docker/generate.sh
@@ -17,7 +17,7 @@ PROJ=${PREFIX}proj:${TAG}
 GEOMETRY=${PREFIX}geometry-chain:${TAG}
 
 docker build --build-arg JAVA_VERSION="${JAVA_VERSION}" -t "${C_BUILDER}" -f ./c-builder/Dockerfile .
-docker build --build-arg JAVA_VERSION="${JAVA_VERSION}" --build-arg TAG="${TAG}" -t "${PROJ}" -f ./proj/Dockerfile .
+docker build --build-arg TAG="${TAG}" -t "${PROJ}" -f ./proj/Dockerfile .
 cd ../java/geometry-chain
 docker build --build-arg JAVA_VERSION="${JAVA_VERSION}" --build-arg TAG="${TAG}" -t "${GEOMETRY}" .
 cd ../../docker
